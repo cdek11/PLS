22d779e568655ac714e270a51b2e295cd900e46d
Jonathan Bryan <jonathan.m.bryan@duke.edu>
2017-05-01 16:27:53 -0400

    Added .py to algo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup(
     name = 'PLS_663_2017',
     version = '0.1',
-    scripts= ['PLS_Algorithm_Optimized'],
+    scripts= ['PLS_Algorithm_Optimized.py'],
     packages = ['Code'],
     license = 'MIT'
 )
