5741d5caab3204f40a159f171d90ae37e30ff436
cdek11 <carmen.dekmezian@duke.edu>
2017-05-01 13:31:28 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from setuptools import setup
+
+setup(
+    name = 'PLS_663_2017'
+    version = '0.1'
+    scripts= ['PLS_Algorithm_Optimized']
+    packages = ['Code']
+    license = 'MIT'
+)
+
